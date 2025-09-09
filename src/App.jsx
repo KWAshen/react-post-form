258722993728d740db9d03b20601aa26ca669e70
KWAshen <ashen.maliaha@gmail.com>
2025-09-09 21:50:39 +0200

    import , onchange

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [formdata, setformdata] = useState({
+    author:"",
+    title:"",
+    public:false,
+    body:"",
+  })
 
+const handleChange = () => {
+  console.log(formdata);
+}
 
   return (
     <div className="container my-5">
@@ -12,28 +23,43 @@ function App() {
         <form>
           <div className="row">
             <div className="col-12 col-md-4">
-              <form action="" className="form-label">Author</form>
-              <input type="text" className="form-control" placeholder="Autore" />
+              <form action="" className="form-label">author</form>
+              <input type="text"
+              name="author"
+              value={formdata.author}
+              className="form-control" placeholder="autore"
+              onChange={handleChange}/>
+              
             </div>
           </div>
           <div className="row">
             <div className="col-12 col-md-4">
               <form action="" className="form-label">Title</form>
-              <input type="text" className="from-control" placeholder="Title" />
+              <input type="text"
+              name="title"
+              value={formdata.title}
+              className="from-control" placeholder="Title" onChange={handleChange}/>
             </div>
           </div>
           <div className="row">
             <div className="col-12 col-md-4">
-              <form action="" className="form-label">Body</form>
-              <input type="check-box" className="form-check-input " />
+              <form action="" className="form-label">public</form>
+              <div>
+              <input type="check-box"
+              name="public"
+              checked={formdata.public}
+              className="form-check " />
+              </div>
             </div>
             <div className="col-12">
               <label htmlFor="form-lable">text
               </label>
-              <textarea name="" id="" className="form-control" rows="4" placeholder="text"></textarea>
+              <textarea name="body" id="body"
+              value={formdata.body}
+              className="form-control" rows="4" placeholder="text" onChange={handleChange}></textarea>
             </div>
             <div className="col-12">
-              <button className="btn btn-sucess"></button>
+              <button className="btn btn-success">Save</button>
             </div>
           </div>
         </form>
